7e5d554c80ae25d3ee67273e0b096217b5f53bfd
Simon Shi <simon.shi@ibm.com>
2025-06-11 22:08:39 -0400

    live/ready

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hello_world():
     return 'hello world!'
 
+@app.route('/live')
+def live():
+    return 'live!'
+
+@app.route('/ready')
+def ready():
+    return 'ready!'
+
 if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain('cert.pem', 'key.pem')
