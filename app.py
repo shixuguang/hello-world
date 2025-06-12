6eceba93ab6902698140922f24604c7871e06bfb
Simon Shi <simon.shi@ibm.com>
2025-06-12 13:29:43 -0400

    update

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def ready():
 if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain('cert.pem', 'key.pem')
-    app.run(host='0.0.0.0', port=8443, ssl_context=context) 
+    app.run(host='0.0.0.0', port=8443, ssl_context=context, debug=True) 
